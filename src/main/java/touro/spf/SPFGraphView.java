d5c747d2936bcbc999c03545a64f2599309c3a04
Devora Nagel <devoranagel@gmail.com>
2020-11-13 15:20:02 -0600

    added frame and view

--- src/main/java/touro/spf/SPFGraphView.java
+++ src/main/java/touro/spf/SPFGraphView.java
@@ -1,4 +1,52 @@
 package touro.spf;
 
-public class SPFGraphView {
+import javax.swing.*;
+import java.awt.*;
+import java.util.HashSet;
+
+public class SPFGraphView extends JComponent {
+
+    Graph graph;
+    private int WIDTH_HEIGHT = 50;
+
+    public SPFGraphView(Graph graph) {
+        this.graph = graph;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        paintGraph(g);
+    }
+
+    private void paintGraph(Graphics g) {
+        HashSet<Node> nodes = new HashSet<>();
+
+        for(Node node: nodes) {
+            g.setColor(Color.BLACK);
+            //plot nodes
+            g.fillOval(getNodeX(),getNodeY(),WIDTH_HEIGHT,WIDTH_HEIGHT);
+
+            //find node's connection and draw line between the 2 nodes
+            for(Node connection : node.connections) {
+                g.drawLine(getNodeX(),getNodeY(),getNodeX() + 20, getNodeY() + 20);
+            }
+
+            //if this node is the SPF, color it a diff color
+            if(node == graph.getSPF()) {
+                g.setColor(Color.CYAN);
+                g.fillOval(getNodeX(),getNodeY(),WIDTH_HEIGHT,WIDTH_HEIGHT);
+            }
+        }
+    }
+
+    private int getNodeX() {
+        int x = 50;
+        return x;
+    }
+
+    private int getNodeY() {
+        int y = 50;
+        return y;
+    }
 }
